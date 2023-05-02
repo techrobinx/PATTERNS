f74cb3b741e2c58e945d9df94b4a8ca5d30cb949
Robin <ashchaurasiya06@gmail.com>
2023-05-02 20:22:16 +0530

    add new pattern

--- pattern8.c
+++ pattern8.c
@@ -1,50 +1,49 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int row,space, i, j ;
+int main()
+{
+    int row, space, i, j;
     printf("Enter the number of row \n");
     scanf("%d", &row);
-    row=row-1;
-  int   n=row;
-    for ( i = 0; i <=row; i++)
+    row = row - 1;
+    int n = row;
+    for (i = 0; i <= row; i++)
     {
-        for ( space = row*2; space>=i; space--)
+        for (space = row * 2; space >= i; space--)
         {
             printf(" ");
         }
-        for(j=0; j <=i*2; j++){
-        
-          
-            printf("*");        
+        for (j = 0; j <= i * 2; j++)
+        {
 
+            printf("*");
         }
-       
-     
+
         printf("\n");
     }
-     for ( i = 0; i <=row; i++)
+    for (i = 0; i <= row; i++)
     {
-        for ( space = row; space>i; space--)
+        for (space = row; space > i; space--)
         {
             printf(" ");
         }
-        for(j=0; j <=i*2; j++){
-        
-            printf("*");
-          
-        }
-        for(j=0; j<=n*2; j++){
-           
-            printf(" ");
+        for (j = 0; j <= i * 2; j++)
+        {
 
+            printf("*");
         }
-         for(j=0; j <=i*2; j++){
-            printf("*");        
+        for (j = 0; j <= n * 2; j++)
+        {
+
+            printf(" ");
+        }
+        for (j = 0; j <= i * 2; j++)
+        {
+            printf("*");
         }
         n--;
         printf("\n");
     }
-       
+
     return 0;
-}    
-  
+}

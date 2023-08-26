579b8f45ff26864f8061e7e072998a9c59d6ecf7
Robin <ashchaurasiya06@gmail.com>
2023-08-26 11:31:21 +0530

    format lins

--- pattern13.c
+++ pattern13.c
@@ -1,34 +1,34 @@
 /*
-    *
-   * *
-  *   *
- *     *
-*********
+ Enter the number of row
+6
+     *
+    ***
+   *****
+  *******
+ *********
+***********
 */
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int row,space, i, j ;
+int main()
+{
+    int row, space, i, j;
     printf("Enter the number of row \n");
     scanf("%d", &row);
-    row=row-1;
-    for ( i = 0; i <=row; i++)
+    row = row - 1;
+    for (i = 0; i <= row; i++)
     {
-        for ( space = row; space>i; space--)
+        for (space = row; space > i; space--)
         {
             printf(" ");
         }
-        for(j=0; j <=i*2; j++){
-        
-         
+        for (j = 0; j <= i * 2; j++)
+        {
+
             printf("*");
-
-          
-
         }
         printf("\n");
     }
-       
+
     return 0;
-}    
-  
+}
--- pattern14.c
+++ pattern14.c
@@ -1,29 +1,41 @@
+// Enter the number of row
+// 6
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+// * * * * * *
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+#include <stdio.h>
 
-#include<stdio.h>
-
-int main(){
-    int row,space, i, j ;
+int main()
+{
+    int row, space, i, j;
     printf("Enter the number of row \n");
     scanf("%d", &row);
-    row=row-1;
-    for ( i = 0; i <row; i++)
+    row = row - 1;
+    for (i = 0; i < row; i++)
     {
-    
-        for(j=0; j <=i; j++){
-           printf("* ");
-        
+
+        for (j = 0; j <= i; j++)
+        {
+            printf("* ");
         }
         printf("\n");
     }
-        for(i=row; i>=0; i--){
-            for(j=i; j>=0; j--){
-                printf("* ");
-        
+    for (i = row; i >= 0; i--)
+    {
+        for (j = i; j >= 0; j--)
+        {
+            printf("* ");
         }
-           printf("\n");
-        
-        }
-       
+        printf("\n");
+    }
+
     return 0;
-}    
-  
+}
